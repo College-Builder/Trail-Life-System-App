f445d62eedfdf5fcf470a63e81ce797d554334cd
Gabriel Flores <108844543+GabrielFlores8227@users.noreply.github.com>
2023-11-26 20:55:53 -0300

    Update util.php

--- trail-life/trail-life/s/system/administracao/login/php/login/util.php
+++ trail-life/trail-life/s/system/administracao/login/php/login/util.php
@@ -8,12 +8,15 @@ require_once BASE_DIR . "global-modules/cypher/cypher.php";
 
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_DIR);
 $dotenv->load();
-
+?>
+<?php
+$requestHandler = new RequestHandler();
+?>
+<?php
 $host = $_ENV["SQL_HOST_ADMINISTRACAO_LOGIN"];
 $user = $_ENV["SQL_USER_ADMINISTRACAO_LOGIN"];
 $password = $_ENV["SQL_PASSWORD_ADMINISTRACAO_LOGIN"];
 $database = $_ENV["SQL_DATABASE_ADMINISTRACAO_LOGIN"];
 
 $mysql = new Mysql($host, $user, $password, $database);
-$requestHandler = new RequestHandler();
 ?>
