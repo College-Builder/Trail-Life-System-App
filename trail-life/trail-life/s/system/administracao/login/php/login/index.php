91add6f0aa28f3b7aee50393a7ea0eaa381b1210
Gabriel Flores <108844543+GabrielFlores8227@users.noreply.github.com>
2023-11-26 21:46:31 -0300

    Update index.php

--- trail-life/trail-life/s/system/administracao/login/php/login/index.php
+++ trail-life/trail-life/s/system/administracao/login/php/login/index.php
@@ -1,8 +1,8 @@
 <?php include("./util.php") ?>
 <?php
 try {
-  if (!($_SERVER["REQUEST_METHOD"] == "POST")) {
-    $requestHandler::throwReqException(405, 'Método Não Permitido. Por favor, utilize uma requisição POST.');
+  if (!($_SERVER["REQUEST_METHOD"] == "POST") && !($_SERVER["REQUEST_METHOD"] == "GET")) {
+    $requestHandler::throwReqException(405, 'Método Não Permitido. Por favor, utilize uma requisição GET ou POST.');
   }
 
   $usuario = $_POST["usuario"];
